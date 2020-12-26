eaa9c4ca23d6ccba001f30378baf23f67d25ea26
Dmitry Vorozhbicki <arolij.corp@gmail.com>
2020-12-27 01:58:55 +0300

    login progress

--- lib/Pages/Login/Login.dart
+++ lib/Pages/Login/Login.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -38,16 +40,21 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
-  final _firstNameTextController = TextEditingController();
-  final _lastNameTextController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
   double _formPadding = 24.0;
   double _fieldPadding = 8.0;
 
+  bool _success;
+  String _userEmail;
+
   @override
   Widget build(BuildContext context) {
     return Form (
+      key: _formKey,
       child: Padding(
         padding: EdgeInsets.all(_formPadding),
         child: Column (
@@ -60,15 +67,32 @@ class _LoginFormState extends State<LoginForm> {
             Padding(
               padding: EdgeInsets.all(_fieldPadding),
               child: TextFormField(
-                controller: _firstNameTextController,
+                controller: _emailController,
                 decoration: InputDecoration(hintText: 'Email'),
+                validator: (String value) {
+                  bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
+                  if (!emailValid || value.isEmpty) {
+                    return 'A valid email is required';
+                  }
+                  return null;
+                }
               ),
             ),
             Padding(
               padding: EdgeInsets.all(_fieldPadding),
               child: TextFormField(
-                controller: _lastNameTextController,
+                controller: _passwordController,
                 decoration: InputDecoration(hintText: 'Password'),
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'A password is required';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 characters';
+                  }
+                  return null;
+                },
+                obscureText: true
               ),
             ),
             Padding(
@@ -78,7 +102,11 @@ class _LoginFormState extends State<LoginForm> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(32.0),
                 ),
-                onPressed: _handleLoginButton,
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    _signInWithEmailAndPassword();
+                  }
+                },
                 child: Padding(
                   padding: EdgeInsets.only(left: _formPadding, right: _formPadding, top: _fieldPadding, bottom: _fieldPadding),
                   child: Text('Sign in', 
@@ -86,13 +114,51 @@ class _LoginFormState extends State<LoginForm> {
                 ),
               ), 
             ),
+            Container(
+              alignment: Alignment.center,
+              child: Padding(
+              padding: EdgeInsets.only(top: _success == null ? 0 : _formPadding),
+              child: Text(
+                  _success == null
+                      ? ''
+                      : (_success
+                      ? 'Successfully signed in ' + _userEmail
+                      : 'Sign in failed'),
+                  style: TextStyle(color: Colors.red[300]),
+                )
+              )
+            )
           ],
         ),
       ),
     );
   }
 
-  void _handleLoginButton() {
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
+void _signInWithEmailAndPassword() async {
+  print("Sign in");
+    final FirebaseUser user = (await _auth.signInWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    )).user;
+  
+    if (user != null) {
+      setState(() {
+        _success = true;
+        _userEmail = user.email;
+      });
+    } else {
+      setState(() {
+        _success = false;
+      });
+    }
+      print("Signed in with: ");
+      print(_success);
   }
 }
