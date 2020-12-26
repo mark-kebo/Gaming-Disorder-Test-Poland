2ebc915fa506a1ed85a7ba7eb7a0875d793d6c1e
Dmitry Vorozhbicki <arolij.corp@gmail.com>
2020-12-26 23:56:02 +0300

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
-
 import 'package:flutter/material.dart';
 import 'package:myapp/Pages/Login/Login.dart';
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(LoginPage());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dashboard - Gaming Disorder Test Poland',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
 }
